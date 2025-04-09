1b99f44e3e04262e9afe6d944b2d90d6c43b6cce
mtri04 <minhtritamm@gmail.com>
2025-04-09 12:35:26 +0700

    [update AI]

--- 12_Weboto/obj/Debug/net9.0/12_Weboto.AssemblyInfo.cs
+++ 12_Weboto/obj/Debug/net9.0/12_Weboto.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("12_Weboto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59ac48a535739bb7d16e2bd6a5610288ebb66b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351b477c47169e543d281d024891e1f8ff13a2c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("12_Weboto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("12_Weboto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
